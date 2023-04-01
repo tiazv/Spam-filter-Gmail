2094a1dc66832e836dd5a6b33054be8d5c47f34d
Tia <tia.zvajker@student.um.si>
2023-04-01 14:07:32 +0200

    graficni vmesnik

---
+++ SpamDetector.py
@@ -0,0 +1,45 @@
+import pickle
+import streamlit as st
+from win32com.client import Dispatch
+
+def speak(prediction):
+    speak = Dispatch(("SAPI.SpVoice"))
+    speak.Speak(prediction)
+
+model = pickle.load(open("spam.pkl", "rb"))
+cv = pickle.load(open("vectorizer.pkl", "rb"))
+
+def main():
+    st.title("Email Spam Classification App")
+    msg = st.text_input("Enter mail:")
+    if st.button("Predict"):
+        data = [msg]
+        vect = cv.transform(data).toarray()    
+        prediction = model.predict(vect)
+        result = prediction[0]
+        result_probs = model.predict_proba(vect)[0]
+        spam_percent = result_probs[1] * 100
+        ham_percent = result_probs[0] * 100
+        if result == 1:
+            speak("This is spam mail")
+            if spam_percent > 60:
+            	st.error("This is spam mail")
+            	st.error("Spam percentage: {:.2f} %".format(spam_percent))
+            	st.error("Ham percentage: {:.2f} %".format(ham_percent))
+            elif spam_percent > 40:
+            	st.error("This is spam mail")
+            	st.warning("Spam percentage: {:.2f} %".format(spam_percent))
+            	st.warning("Ham percentage: {:.2f} %".format(ham_percent))
+        else:
+            speak("This is ham mail")
+            if ham_percent > 60:
+            	st.success("This is ham mail")
+            	st.success("Spam percentage: {:.2f} %".format(spam_percent))
+            	st.success("Ham percentage: {:.2f} %".format(ham_percent))
+            elif ham_percent > 40:
+            	st.success("This is ham mail")
+            	st.warning("Spam percentage: {:.2f} %".format(spam_percent))
+            	st.warning("Ham percentage: {:.2f} %".format(ham_percent))
+
+
+main()
--- SpamFilter.py
+++ SpamFilter.py
@@ -4,83 +4,69 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+import pickle
 
 #import data from csv
-spam_df = pd.read_csv("spam.csv")
-spam_df = spam_df.sample(frac=1).reset_index(drop=True)
+data = pd.read_csv("spam.csv")
+data = data.sample(frac=1).reset_index(drop=True)
 
 #add new column
-spam_df["spam"] = spam_df["Category"].apply(lambda x: 1 if x == "spam" else 0)
+data["spam"] = data["Category"].apply(lambda x: 1 if x == "spam" else 0)
 
 #PRINT: how many spam/ham, how many different, most common, how many most common
-print(spam_df)
-print(spam_df.groupby("Category").describe())
-print("------------------------------------------------")
+#print(data)
+#print(data.groupby("Category").describe())
+#print("------------------------------------------------")
 
-pod = spam_df.Message #content
-rez = spam_df.spam #spam or ham
+cv = CountVectorizer()
 
-#create train/test split
+pod = data.Message #content
+rez = data.spam #spam or ham
 pod_train, pod_test, rez_train, rez_test = train_test_split(pod, rez, test_size=.01)
 
 #PRINT: inspect data
-print("pod_train")
+pod = cv.fit(pod)
 print(pod_train)
 print(pod_train.describe())
 print("------------------------------------------------")
 
 #find word count and store data as a matrix
-cv = CountVectorizer()
+
 pod_train_count = cv.fit_transform(pod_train.values)
 
 #PRINT: how many times does a word show up
-print("pod_train_count")
-print(pod_train_count.toarray())
+#print(pod_train_count.toarray())
 print("------------------------------------------------")
 
 #train model
 model = MultinomialNB()
 model.fit(pod_train_count, rez_train)
 
-#ham example for prediction
-email_ham = ["meeting reward click"]
-email_ham_count = cv.transform(email_ham)
-email_ham_test = model.predict(email_ham_count)
+pickle.dump(model, open("spam.pkl", "wb"))
+pickle.dump(cv, open("vectorizer.pkl", "wb"))
+clf = pickle.load(open("spam.pkl", "rb"))
 
-#spam example for prediction
-email_spam = ["reward money click movie energy meeting"]
-email_spam_count = cv.transform(email_spam)
-email_spam_test = model.predict(email_spam_count)
+#message for prediction
+msg = ["hi i am a nigerian prince please be my nigerian princess click here for money"]
+msg_count = cv.transform(msg)
+result = model.predict(msg_count)
 
-#PRINT: spam and ham prediction
-print(email_spam_count)
-print("should be ham (0):")
-print(model.predict(email_ham_count))
-print("should be spam (1):")
-print(model.predict(email_spam_count))
+print("Result of prediction")
+print(result)
 print("------------------------------------------------")
 
 #test model
 pod_test_count = cv.transform(pod_test)
-model.score(pod_test_count, rez_test)
+accuracy1 = model.score(pod_test_count, rez_test)
+accuracy2 = str(model.score(pod_test_count, rez_test) * 100)
+accuracy2_str = "{:.2f}".format(float(accuracy2))
 
-#PRINT: accuracy
-print(model.score(pod_test_count, rez_test))
-print(model.score(email_ham_count, email_ham_test))
-print(model.score(email_spam_count, email_spam_test))
-# Predict the class probabilities for the email
-email_ham_test_probs = model.predict_proba(email_ham_count)[0]
-email_spam_test_probs = model.predict_proba(email_spam_count)[0]
-# Calculate the percentage of spam and ham in the predicted labels
-spam_percent = email_ham_test_probs[1] * 100
-ham_percent = email_ham_test_probs[0] * 100
+print("Accuracy of filter: " + accuracy2_str +  " %")
+#print(model.score(msg_count, result))
 
-spam_percent1 = email_spam_test_probs[1] * 100
-ham_percent1 = email_spam_test_probs[0] * 100
-
-# Print the results
-print(f"Spam percentage: {spam_percent:.2f}%")
-print(f"Ham percentage: {ham_percent:.2f}%")
-
-print(f"Spam percentage1: {spam_percent1:.2f}%")
-print(f"Ham percentage1: {ham_percent1:.2f}%")
+#PROCENTI
+result_probs = model.predict_proba(msg_count)[0]
+spam_percent = result_probs[1] * 100
+ham_percent = result_probs[0] * 100
+print(f"Spam percentage: {spam_percent:.2f} %")
+print(f"Ham percentage: {ham_percent:.2f} %")
